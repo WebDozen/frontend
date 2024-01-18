50468d779689aeebae050b190c221fa33b03fb7b
Dmitry Tokmak <dimka.listok@gmail.com>
2024-01-18 13:55:54 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="header"></header>
+      <footer className="footer"></footer>
+    </div>
+  )
+}
+
+export default App
