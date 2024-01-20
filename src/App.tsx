e247a0307a80c39c10dcf4f8312debf175f67418
Dmitry Tokmak <dimka.listok@gmail.com>
2024-01-20 16:56:07 +0300

    черновик

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
 import { Outlet, Route, Routes } from "react-router-dom"
 import style from "./App.module.scss"
-import Home from "./pages/Home/Home"
+// import Home from "./pages/Home/Home"
+import appStyle from "./App.module.scss"
+import ManagerPage from "./pages/manager-page/ManagerPage"
 
 const App = () => {
   return (
-    <Routes>
-      {/* 1 уроверь */}
-      <Route
-        path="/"
-        element={
-          <>
-            <header className={style.header}>Привет</header>
-            <Outlet />
-            <footer className="footer">Пока</footer>
-          </>
-        }
-      >
-        {/* 2 уроверь */}
+    <div className={appStyle.app}>
+      <Routes>
+        {/* 1 уроверь */}
         <Route
-          index
+          path="/"
           element={
-            <div className={style.App}>
-              <Home />
-            </div>
+            <>
+              <header className={style.header}>Привет</header>
+              <div className={style.main}>
+                <Outlet />
+              </div>
+              <footer className="footer">Пока</footer>
+            </>
           }
-        />
+        >
+          {/* 2 уроверь */}
+          <Route index element={<ManagerPage />} />
 
-        <Route path="/employee/:id" element={<div>Картчока сотрудника</div>} />
-        
-      </Route>
-    </Routes>
+          <Route
+            path="/employee/:id"
+            element={<div>Картчока сотрудника</div>}
+          />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
