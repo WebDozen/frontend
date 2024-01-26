559ae8172759369ccb355a54554d93e5e4c3ffc4
Roman Bondarev <Rbondarev22@gmail.com>
2024-01-26 17:52:12 +0700

    small fix

--- src/pages/EmployeePage/EmployeePage.tsx
+++ src/pages/EmployeePage/EmployeePage.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import IdpList from "../../components/IdpList/IdpList";
 import EmployeeCard from "../../components/EmployeeCard/EmployeeCard";
 import PlateSuccess from "../../components/PlateSuccess/PlateSuccess";
@@ -6,7 +5,7 @@ import NewPlanMessage from "../../components/NewPlanMessage/NewPlanMessage";
 import PlateFail from "../../components/PlateFail/PlateFail";
 
 const EmployeePage = () => {
-  const [activeIPRs, setActiveIPRs] = useState(false);
+  const activeIPRs = false;
 
   return (
     <div>
