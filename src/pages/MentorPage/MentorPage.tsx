6d070df9c8c1959ec1691e858b4a388aee05b638
Dmitry Tokmak <dimka.listok@gmail.com>
2024-02-05 07:54:34 +0300

    fix is_mentor

--- src/pages/MentorPage/MentorPage.tsx
+++ src/pages/MentorPage/MentorPage.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getEmployeeByID, getEmployees, getIdps } from "../../services/actions";
 import { useAppDispatch, useAppSelector } from "../../services/hook";
-import { getEmployeeData } from "../../services/selectors";
+import { getEmployeeData, getUserData } from "../../services/selectors";
 import EmployeeCard from "../../components/EmployeeCard/EmployeeCard";
 import TabsCustomMentor from "../../components/TabsCustomMentor/TabsCustomMentor";
 import { Gap } from "../../components/ui-kit";
@@ -22,9 +22,10 @@ const MentorPage = () => {
     dispatch(getIdps(id));
   }, [dispatch]);
 
-  const {
-    employee: { is_mentor },
-  } = useAppSelector(getEmployeeData);
+  // const {
+  //   employee: { is_mentor },
+  // } = useAppSelector(getEmployeeData);
+  const { is_mentor } = useAppSelector(getUserData);
 
   return (
     <div className={styles.content}>
@@ -33,27 +34,6 @@ const MentorPage = () => {
       <Gap size="2xl" />
       {is_mentor && <TabsCustomMentor />}
       <Gap size="5xl" />
-      {/* !! если все выполнены или отменены,то показываем зеленую плашку только тогда.
-       нужно будет переделать !!
-      {status === TYPE_SLAG_IDP.completed && (
-        <PlateWrapper
-          config={plateSuccess}
-          view="positive"
-          titleText="Сотрудник выполнил все ИПР"
-          text="Пришло время создать новый план развития и двигаться к новым целям!"
-        />
-      )}
-      {status === "expired" && (
-        <PlateWrapper
-          config={plateAttention}
-          view="attention"
-          titleText="Сотрудник не выполнил последний ИПР"
-          text="Возможно, задач было слишком много? Узнайте у сотрудника, что пошло не так, и составьте новый план для развития"
-        />
-      )}
-
-      {employee.idp.total_idp_count === 0 ? <NewPlanMessage /> : <IdpList />}
- */}
     </div>
   );
 };
