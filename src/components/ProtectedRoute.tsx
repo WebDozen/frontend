a42a477c952ab740c69cd2f38e5aae804742e1f7
Dmitry Tokmak <dimka.listok@gmail.com>
2024-02-05 08:39:09 +0300

    fix status

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -12,7 +12,11 @@ const ProtectedRoute: React.FC<PropsWithChildren> = ({ children }) => {
     return <Navigate to="/start" replace />;
   }
   if (role !== "manager" && location.pathname === "/") {
-    return is_mentor ? <Navigate to={`/mentor/employee/${id}`} replace /> : <Navigate to={`/employee/${id}`} replace />;
+    return is_mentor ? (
+      <Navigate to={`/mentor/employee/${id}`} replace />
+    ) : (
+      <Navigate to={`/employee/${id}`} replace />
+    );
   }
   return children;
 };
